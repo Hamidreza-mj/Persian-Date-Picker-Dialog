1bd5a589ecff34e5ffe73bfac7f755ade6f089cd
hamidreza_mj <hamid.mj313@gmail.com>
2021-11-05 01:34:21 +0330

    avoid dialog dismiss after select date

--- persiandatepicker/src/main/java/ir/hamsaa/persiandatepicker/PersianDatePickerDialog.java
+++ persiandatepicker/src/main/java/ir/hamsaa/persiandatepicker/PersianDatePickerDialog.java
@@ -428,7 +428,7 @@ public class PersianDatePickerDialog {
                 if (persianPickerListener != null) {
                     persianPickerListener.onDateSelected(datePickerView.getPersianDate(), isPassed);
                 }
-                dialog.dismiss();
+//                dialog.dismiss();
             }
         });
 
